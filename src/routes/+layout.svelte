5fb61231ce8f2b7ecd8286a98484924aed771a5b
Alvaro Raditya Soewardono <39083254+alvaroswdn@users.noreply.github.com>
2025-07-08 15:49:12 +0700

    Change mobile padding and color contrasts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,7 +31,7 @@
 
 <div class="bg-background grid text-primary font-primary text-xl h-full min-h-screen">
 	<div class="h-full max-w-6xl m-auto px-8">
-		<header class="flex flex-col gap-3 items-center justify-between py-7 lg:flex-row">
+		<header class="flex flex-col gap-4 items-center justify-between py-7 lg:flex-row">
 			<a href="/" class="flex items-center font-accent text-4xl font-semibold">
 				alvaro<span class="text-accent">.</span>
 			</a>
@@ -47,7 +47,7 @@
 			</div>
 		</header>
 
-		<main class="px-6 pb-32">
+		<main class="px-2 pb-32 lg:px-6">
 			{@render children()}
 		</main>
 	</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,27 +17,27 @@
 	<a
 		href={url}
 		target="_blank"
-		class="group flex items-center justify-between px-7 py-6 hover:text-accent hover:scale-[102%] transition-all duration-400 ease-out"
+		class="group flex items-center justify-between px-5 py-4 hover:text-accent hover:scale-[102%] transition-all duration-400 ease-out lg:px-7 lg:py-6"
 	>
 		{title}
 		<LinkIcon />
 	</a>
 {/snippet}
 
-<div class="flex flex-col gap-24 mt-8 lg:mt-20 lg:flex-row lg:gap-36">
+<div class="flex flex-col gap-16 lg:mt-20 lg:flex-row lg:gap-36">
 	<section class="flex flex-col gap-8 flex-2/5 mt-6">
 		<div>
 			<h1 class="text-4xl font-accent font-semibold">Alvaro R Soewardono</h1>
-			<span class="text-xl text-accent/75 font-semibold">Fullstack Developer</span>
+			<span class="text-xl text-accent font-semibold">Fullstack Developer</span>
 		</div>
 
-		<p class="text-justify pr-8">
+		<p class="text-justify lg:pr-8">
 			I'm a highly committed learner with a growing interest in the field of technology. With an
 			extensive list of skills, seeking to continuously gain new experiences all over the world.
 		</p>
 
 		<enhanced:img
-			class="rounded-lg w-6/7 mt-4 bg-slate-200"
+			class="rounded-lg w-full lg:w-6/7 mt-4 bg-slate-200"
 			src="./scene.webp"
 			alt="scene"
 			draggable="false"
@@ -45,7 +45,7 @@
 		/>
 	</section>
 	<section class="flex-3/5">
-		<h2 class="mb-2 text-primary/60 font-medium">Recent Projects</h2>
+		<h2 class="mb-2 text-primary/80 font-medium">Recent Projects</h2>
 		<div class="grid outline">
 			{@render project('Lucky Network', 'https://luckynetwork.net')}
 			<hr class="h-px" />
