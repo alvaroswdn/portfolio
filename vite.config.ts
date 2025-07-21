34349998e581fd6f810f422528c876cbe64e1120
Alvaro Raditya Soewardono <39083254+alvaroswdn@users.noreply.github.com>
2025-07-21 10:20:17 +0700

    Update dependencies and test experimental features

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import { enhancedImages } from '@sveltejs/enhanced-img'
-import { sveltekit } from '@sveltejs/kit/vite'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		enhancedImages(),
-		sveltekit(),
-		tailwindcss(),
-	],
-})
+	css: {
+		transformer: 'lightningcss'
+	},
+	build: {
+		cssMinify: 'lightningcss'
+	},
+	experimental: {
+		enableNativePlugin: true
+	},
+	plugins: [enhancedImages(), sveltekit(), tailwindcss()]
+});
